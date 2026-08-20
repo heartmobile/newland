58498ac4fbca7680b9ef308f1763d0640fd048e6
heartmobile <john@heartmobile.ca>
2026-08-19 22:24:07 -0400

    Update route.ts

--- app/api/order/route.ts
+++ app/api/order/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { SentrixApiService } from '@/lib/mobile-sentrixapi';
+import { SentrixApiService } from '@/lib/sentrixApi';
 
 export async function GET(request: Request) {
   try {
