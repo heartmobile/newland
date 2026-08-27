3d8686edcbe4f7ee9619f593aec49b4684815faa
heartmobile <john@heartmobile.ca>
2026-08-27 08:29:30 -0400

    Update route.ts

--- app/api/create-customer/route.ts
+++ app/api/create-customer/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { SentrixApiService, CustomerRegistrationInput } from '@/lib/api';
+import { SentrixApiService, CustomerRegistrationInput } from '@/lib/api/sentrix';
 
 export async function POST(request: Request) {
   try {
